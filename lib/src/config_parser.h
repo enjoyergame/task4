2852b6905e285c73a3fac02db73e6f6c391c6f5e
holdderr <newnewfrfr@proton.me>
2026-07-16 01:48:39 +0500

    add config amd .h for parser

---
+++ lib/src/config_parser.h
@@ -0,0 +1,37 @@
+/*
+парсим конфиг
+
+Кучуков Ридаль Радикович
+МК-101
+*/
+#ifndef PASSGEN_CONFIG_PARSER_H
+#define PASSGEN_CONFIG_PARSER_H
+
+#include "passgen/error.h"
+#include "cli_lexer.h"
+#include "charset.h"
+#include <stdbool.h>
+
+#define CONFIG_NOT_SET -1
+
+typedef struct {
+    int minl;
+    int maxl;
+    int n;
+    int c;
+
+    CharsetInfo charset;
+
+    bool has_s;
+    bool has_i;
+    bool has_a;
+    bool has_C;
+} AppConfig;
+
+void config_init(AppConfig *config);
+void config_free(AppConfig *config);
+
+// Парсит список токенов в конфигурацию и проверяет все логические конфликты
+PassgenError config_parse(AppConfig *config, const TokenList *tokens);
+
+#endif // PASSGEN_CONFIG_PARSER_H
