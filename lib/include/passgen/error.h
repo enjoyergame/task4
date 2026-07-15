542e389a7fc78cb192db5ea28486c32bb88286cc
holdderr <newnewfrfr@proton.me>
2026-07-15 23:40:32 +0500

    add errors header

--- lib/include/passgen/error.h
+++ lib/include/passgen/error.h
@@ -3,3 +3,40 @@
 Кучуков Ридаль Радикович
 МК-101
 */
+#ifndef PASSGEN_ERROR_H
+#define PASSGEN_ERROR_H
+
+typedef enum
+{
+    PASSGEN_OK = 0,
+
+    // Ошибки лексера аргументов командной строки 
+    PASSGEN_ERR_MISSING_VALUE,     // у опции нет значения
+    PASSGEN_ERR_UNKNOWN_SEPARATOR, // некорректный синтаксис разделителя
+
+    // Ошибки парсинга значений опций 
+    PASSGEN_ERR_NOT_A_NUMBER,     // значение опции должно быть числом 
+    PASSGEN_ERR_DUPLICATE_OPTION, // опция указана более одного раза 
+    PASSGEN_ERR_DUPLICATE_CHAR,   // повтор символа внутри -C или -a 
+
+    // Ошибки логики/конфликтов конфигурации 
+    PASSGEN_ERR_CONFLICT_N_MINMAX, // -n вместе с -minl/-maxl
+    PASSGEN_ERR_CONFLICT_A_C,      // -a вместе с -C 
+    PASSGEN_ERR_CONFLICT_PAIR_N,   // парная опция вместе с -n
+    PASSGEN_ERR_INCOMPLETE_PAIR,   // есть одна из парных опций без второй
+    PASSGEN_ERR_INCOMPLETE_RANGE,  // задан -minl без -maxl или наоборот
+    // Ошибки вероятностей 
+    PASSGEN_ERR_PROB_OVERFLOW, // сумма вероятностей превышает 1.0
+    PASSGEN_ERR_PROB_INVALID,  // вероятность не в диапазоне [0,1]
+
+    // Общие 
+    PASSGEN_ERR_NO_MEMORY, // ошибка выделения памяти
+    PASSGEN_ERR_UNKNOWN    // запасной вариант
+} PassgenError;
+
+/*
+Возвращает текстовое сообщение, соответствующее коду ошибки.
+*/
+const char *passgen_error_message(PassgenError code);
+
+#endif // PASSGEN_ERROR_H 
