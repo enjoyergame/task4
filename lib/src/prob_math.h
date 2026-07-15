983ec03ee2fc5c1a0e4fbee44778206ded60aefa
holdderr <newnewfrfr@proton.me>
2026-07-16 03:01:16 +0500

    add .h for math

---
+++ lib/src/prob_math.h
@@ -0,0 +1,35 @@
+/*
+модуль математики вероятностей
+
+Кучуков Ридаль Радикович
+МК-101
+*/
+
+#ifndef PASSGEN_PROB_MATH_H
+#define PASSGEN_PROB_MATH_H
+
+#include "passgen/error.h"
+#include "config_parser.h"
+#include "cli_lexer.h"
+#include <stddef.h>
+
+typedef struct
+{
+    char symbol; // символ (из пользовательского алфавита или группа a/A/D/S)
+    double prob; // заданная или вычисленная вероятность [0.0, 1.0]
+    double cdf;  // кумулятивная вероятность (для алгоритма генерации)
+} ProbItem;
+
+typedef struct
+{
+    ProbItem *items;
+    size_t count;
+} ProbTable;
+
+void prob_table_init(ProbTable *pt);
+void prob_table_free(ProbTable *pt);
+
+// Сборка таблицы вероятностей на основе конфига и токенов -p
+PassgenError prob_table_build(ProbTable *pt, const AppConfig *config, const TokenList *tokens);
+
+#endif // PASSGEN_PROB_MATH_H
