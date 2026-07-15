25f12dc97998d3f3768eb5af925d81af27842be4
holdderr <newnewfrfr@proton.me>
2026-07-16 00:04:25 +0500

    minimal main

---
+++ app/main.c
@@ -0,0 +1,17 @@
+/*
+main
+
+ФИО
+Группа
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    printf("passgen: not implemented yet\n");
+    return 0;
+}
