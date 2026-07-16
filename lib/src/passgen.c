f7f9f0df7506e8dfb13d55774bb0b4324ecf20c9
holdderr <newnewfrfr@proton.me>
2026-07-16 05:28:21 +0500

    debug

--- lib/src/passgen.c
+++ lib/src/passgen.c
@@ -13,51 +13,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-PassgenError passgen_run(int argc, char *const argv[])
-{
+PassgenError passgen_run(int argc, char *const argv[]) {
     TokenList tokens = {NULL, 0};
     AppConfig config;
     config_init(&config);
     ProbTable pt;
     prob_table_init(&pt);
-
+    
     PassgenError err = cli_lexer_tokenize(argc, argv, &tokens);
-    if (err != PASSGEN_OK)
-    {
+    if (err != PASSGEN_OK) {
         fprintf(stderr, "%s\n", passgen_error_message(err));
         goto cleanup;
     }
 
     err = config_parse(&config, &tokens);
-    if (err != PASSGEN_OK)
-    {
+    if (err != PASSGEN_OK) {
         fprintf(stderr, "%s\n", passgen_error_message(err));
         goto cleanup;
     }
 
     err = prob_table_build(&pt, &config, &tokens);
-    if (err != PASSGEN_OK)
-    {
+    if (err != PASSGEN_OK) {
         fprintf(stderr, "%s\n", passgen_error_message(err));
         goto cleanup;
     }
 
     generator_init();
-    for (int i = 0; i < config.c; i++)
-    {
-        int len = generator_determine_length(&config);
 
+    for (int i = 0; i < config.c; i++) {
+        int len = generator_determine_length(&config);
+        
         char *buf = (char *)malloc(len + 1);
-        if (buf == NULL)
-        {
+        if (buf == NULL) {
             err = PASSGEN_ERR_NO_MEMORY;
             fprintf(stderr, "%s\n", passgen_error_message(err));
             goto cleanup;
         }
 
         err = generator_create_password(&config, &pt, buf, len + 1);
-        if (err != PASSGEN_OK)
-        {
+        if (err != PASSGEN_OK) {
             fprintf(stderr, "%s\n", passgen_error_message(err));
             free(buf);
             goto cleanup;
