801c50c4799e69f5108388c636c6154d924dd703
holdderr <newnewfrfr@proton.me>
2026-07-16 05:06:29 +0500

    add .h

---
+++ lib/include/passgen/passgen.h
@@ -0,0 +1,15 @@
+/*
+passgen
+
+Кучуков Ридаль Радикович
+МК-101
+*/
+
+#ifndef PASSGEN_H
+#define PASSGEN_H
+
+#include "passgen/error.h"
+
+PassgenError passgen_run(int argc, char *const argv[]);
+
+#endif // PASSGEN_H
